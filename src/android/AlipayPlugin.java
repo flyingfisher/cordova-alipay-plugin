3fd31649b45fb5d4cd955ce42783db9b806cadbc
yuxiang <yuxiang@yuhongtech.com>
2015-05-12 16:20:26 +0800

    use cordova thread pool instead of Thread

--- src/android/AlipayPlugin.java
+++ src/android/AlipayPlugin.java
@@ -20,7 +20,7 @@ public class AlipayPlugin extends CordovaPlugin {
                 @Override
                 public void run() {
                     PayTask task = new PayTask(activity);
-                    boolean isExist = payTask.checkAccountIfExist();
+                    boolean isExist = task.checkAccountIfExist();
                     if(!isExist) return callbackContext.error("wallet not found");
 
                     String payRst = task.pay(payStr);
